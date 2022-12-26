165432010e045377885978ac71ba87383c31fc0e
sdaharegithub <snehaldahare@gmail.com>
2022-12-26 16:50:55 +0530

    first commit:added arraylist demo code

---
+++ src/javaproject/ArrayListDemo.java
@@ -0,0 +1,14 @@
+package javaproject;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+	public static void main(String[] args) {
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("snehal");
+		list.add("achal");
+		list.add("kiran");
+		System.out.println(list);
+	}
+
+}
