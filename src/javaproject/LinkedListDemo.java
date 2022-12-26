ba2087271d81b0aee19df4f4509d80223d800117
sdaharegithub <snehaldahare@gmail.com>
2022-12-26 18:55:17 +0530

    second commit:added code for linkedlist

---
+++ src/javaproject/LinkedListDemo.java
@@ -0,0 +1,14 @@
+package javaproject;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+	public static void main(String[] args) {
+		LinkedList<Integer>list=new LinkedList<Integer>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		System.out.println(list);
+	}
+
+}
